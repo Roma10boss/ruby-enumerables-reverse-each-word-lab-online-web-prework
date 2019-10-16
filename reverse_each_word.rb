8376abf66f4625791e9d9a7a420a2d707a1c984d
Romario Gustave <romagustave10@gmail.com>
2019-10-16 23:03:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 def reverse()
   #first code 
   reverse_each_word= ["Hi, i love FlatIrron School"]
-  puts reverse_each_word.each 
+  reverse_each_word.each 
 end
 
  def collect 
    collect_each_word = ["My Favorite so far"]
-   puts collect_each_word.collect 
+  collect_each_word.collect 
  end
  
   
