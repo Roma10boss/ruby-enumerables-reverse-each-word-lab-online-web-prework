52f85ca267eebb0df0f1465fe8ca00a3414a738b
Romario Gustave <romagustave10@gmail.com>
2019-10-16 23:12:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = word.split("Hi how are you")
   new_array = []
-  new_array = array.collect {|array| word.reverse}
+  new_array = array.collect {|array| array.reverse}
   new_array.joi("Hi how are you")
  end
  
